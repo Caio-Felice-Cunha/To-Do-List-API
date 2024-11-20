5adc3a70e32872906ed502e0600bdfe147f4722b
Caio di Felice Cunha <caiofcunha@hotmail.com>
2024-11-20 07:27:33 -0800

    defning the List, and the optional 0, 1 and 2

--- todoapi.py
+++ todoapi.py
@@ -8,17 +8,31 @@ app = FastAPI()
 
 class Todo(BaseModel):
     task: str
-    done: boolean
+    done: bool
     deadline: Optional[date]
 
 list = []
 
 @app.post('/insert')
 def insert(todo: Todo):
+    try:
+        list.append(todo)
+        return {'status': 'success'}
+    
+    except:
+        return { 'status': 'erro'}
 
 
+@app.post('/list')
+def list(optional: int = 0):
+    if optional == 0:
+        return list
+    elif optional == 1:
+        return list(filter(lambda x: x.done == False, list))
 
+    elif optional == 2:
+        return list(filter(lambda x: x.done == True, list))
 
-
+    
 
 
